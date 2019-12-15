72495a5ab803b956a909b668aa570934147c5d7a
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-12-14 18:43:45 -0800

    added banner message

---
+++ clicky/src/components/banner/banner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './banner.css';
+
+const banner = () => (
+  <div className="banner text-center d-flex align-items-center justify-content-center">
+    <p className="m-0">Click on an image to earn points, be careful not to click on the same image twice!</p>
+  </div>
+);
+export default banner;
