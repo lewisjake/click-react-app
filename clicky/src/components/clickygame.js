3ebd84627faf290b80a8c216ad65a3de0e2be38e
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-12-14 22:01:57 -0800

    building app logic

---
+++ clicky/src/components/clickygame.js
@@ -0,0 +1,7 @@
+
+import React, { Component } from 'react';
+import navbar from './navbar';
+import container from './container';
+import footer from './footer';
+import banner from './banner';
+import images from '../images';
---
+++ clicky/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import clickygame from './components/clickygame';
+import './index.css';
+
+ReactDOM.render(<clickygame />, document.getElementById('root'));
