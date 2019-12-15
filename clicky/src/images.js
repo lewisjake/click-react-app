b6dd3c3d16eeb7b4b330747aa0171da87a183d58
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-12-14 18:14:21 -0800

    imported images for cards

---
+++ clicky/src/images.js
@@ -0,0 +1,25 @@
+import black from "./images/black_ranger.jpeg";
+import blue from "./images.blue_ranger.jpeg";
+import donotello from "./images/donotello.jpg";
+import leonardo from "./images/leonardo.jpeg";
+import michaelangelo from "./images/michaelangelo.jpg";
+import pink from "./images/pink_ranger.jpeg";
+import raphael from "./images/raphael.jpg";
+import red from "./images/red_ranger.jpeg";
+import splinter from "./images/splinter.png";
+import yellow from "./images/yellow_ranger.jpeg";
+
+const images = [
+    black,
+    blue,
+    donotello,
+    leonardo,
+    michaelangelo,
+    pink,
+    raphael,
+    red,
+    splinter,
+    yellow
+];
+
+export default images;
